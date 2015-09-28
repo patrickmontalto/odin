8defb5fd50decc844b2f58f8d0e9311c1425c45e
patrickmontalto <pmontalto@gmail.com>
2015-09-28 15:29:47 -0400

    CRUD functionality for blogger app

--- rails_101/blogger/app/controllers/articles_controller.rb
+++ rails_101/blogger/app/controllers/articles_controller.rb
@@ -5,7 +5,45 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find_by(params[:id])
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article #{@article.title} Created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    flash.notice = "Article Destroyed!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article #{@article.title} Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+
+  private 
+
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 
 end
