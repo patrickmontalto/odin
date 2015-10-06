6a2165f323a00003eb17b420433fb7c3c1da99a5
patrickmontalto <pmontalto@gmail.com>
2015-10-06 12:58:28 -0400

    mastermind with AI

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -7,29 +7,56 @@ class Mastermind
     @game_over = false
     @creator = creator
     @colors = %w(r g b y m c)
+    @board = ["==========================="]
   end
 
   # Game will run until game_over is set to true
   def start_game
     code_guessed = false
     if @player.is_creator?
+      @s = %w(r g b y c m).repeated_permutation(4).to_a
+      @s[0], @s[7] = @s[7], @s[0]
+      puts "Enter your secret code! Pick 4: (R)ed, (G)reen, (Y)ellow, (B)lue, (M)agenta, and (C)yan:"
+      @code = gets.chomp.downcase.scan(/[rgybmc]/).map {|n| n}
+      @code_guess = []
       while !@game_over
-        break # Debugging
+        computer_guess
+        if @code == @code_guess
+          puts "The computer guessed the code correctly!"
+          puts "The code was #{@code}"
+          @game_over = true
+        else
+          results = guess_feedback(@code_guess)
+          @s.delete_if {|possible_guess| guess_feedback(possible_guess) == results}
+          print_board
+          @turns -=1
+        end
+        sleep(1)
+        puts "#{@turns} left for the CPU!" unless @game_over || @turns == 0
+        if @turns == 0
+          puts "The computer ran out of guesses! The code was #{@code}!"
+          @game_over = true
+        end
       end
     else
       generate_code
-      while !@game_over || @turns > 0
+      while !@game_over
         guess
-        code_guessed = check_guess(@code_guess)
         if @code == @code_guess
           puts "You guessed correctly!"
+          puts "The code was #{@code}"
           @game_over = true
         else
           guess_feedback(@code_guess)
+          print_board
           @turns -=1
         end
+        puts "#{@turns} guesses left!" unless @game_over || @turns == 0
+        if @turns == 0
+          puts "You ran out of guesses! The code was #{@code}!"
+          @game_over = true
+        end
       end
-      puts "You ran out of guesses!" if @turns == 0
     end
   end
 
@@ -39,48 +66,53 @@ class Mastermind
 
   def guess
     puts "What's the code, eh? Pick 4: (R)ed, (G)reen, (Y)ellow, (B)lue, (M)agenta, and (C)yan:"
-    @code_guess = gets.chomp.downcase
+    @code_guess = gets.chomp.downcase.scan(/[rgybmc]/).map {|n| n}
+    return @code_guess if @code_guess.size == 4
+    puts "That code does not meet the criteria. Please try again"
+    return guess
   end
 
   def guess_feedback(guess)
-    indexes = [0,1,2,3,4,5]
-    black = 0
-    white = 0
+    indexes = [0,1,2,3]
+    @black = 0
+    @white = 0
     leftover_code = []
     leftover_guess = []
     @code.each_with_index do |elem, index|
-      if code[index] == guess[index]
-        black += 1
+      if @code[index] == guess[index]
+        @black += 1
         indexes.delete(index)
       end
     end
 
     indexes.each do |index|
-      leftover_code << code[index]
-      letover_guess << guess[index]
+      leftover_code << @code[index]
+      leftover_guess << guess[index]
     end
 
-    # leftover_code = [r,r,g]
-    # leftover_guess = [g,g,r] => [g,r]
-    # returns white = 2
-
-    # leftover_code = [r,g,g,c]
-    # leftover_guess = [g,r,r,b] => [g,r,b]
-    # returns white = 2
-
-    # leftover_code = [r,r,g,g]
-    # lefover_guess = [g,g,r,r] => flatten to [g,r] would only return 2!
-    # returns white = 4
-    # need a technique such as r => 2, g => 2
-    # g => 2 r => 2 if g == g then white += g etc.
-    # count number of each elements and store in hash ?
-    guess.each do |elem|
-      white += 1 if leftover_code.include?(elem)
+    leftover_guess.each do |n|
+      if leftover_code.include?(n)
+        @white += 1
+        leftover_code.delete_at(leftover_code.index(n))
+      end
     end
-    # If guess[0] == code[0] => B += 1
-    # ..
-    # If guess[5] == code[5] => B += 1
+    return [@black, @white]
+  end
 
+  def print_board
+    puts "Current board: "
+    result = @code_guess.join(' ')
+    @board << "|##{@turns}| [#{result}] - [B-#{@black} W-#{@white}]"
+    @board << "==========================="
+    @board.each do |row|
+      puts row
+    end
+  end
+
+  def computer_guess
+    @code_guess = @s[0]
+    puts "Computer is making a guess of #{@code_guess}!"
+    @code_guess
   end
 
 end
