c19c125eb6ec9f0aaf287baafe9f8c0f4a83cc74
patrickmontalto <pmontalto@gmail.com>
2015-10-27 09:40:15 -0400

    Remove comments and posts from user

---
+++ active_record/micro-reddit/db/migrate/20151027133920_remove_comments_and_posts_from_user.rb
@@ -0,0 +1,6 @@
+class RemoveCommentsAndPostsFromUser < ActiveRecord::Migration
+  def change
+    remove_column :users, :comment_id
+    remove_column :users, :post_id
+  end
+end
--- active_record/micro-reddit/db/schema.rb
+++ active_record/micro-reddit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026204400) do
+ActiveRecord::Schema.define(version: 20151027133920) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 20151026204400) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.integer  "post_id"
-    t.integer  "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
