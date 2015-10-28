a6a9e58d21563ed5a40b51077dc40b3c0ca3ec7c
patrickmontalto <pmontalto@gmail.com>
2015-10-28 19:03:42 -0400

    fix users

--- active_record/micro-reddit/db/migrate/20151026203444_create_users.rb
+++ active_record/micro-reddit/db/migrate/20151026203444_create_users.rb
@@ -2,9 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.integer :post_id
-      t.integer :comment_id
-
       t.timestamps null: false
     end
   end
