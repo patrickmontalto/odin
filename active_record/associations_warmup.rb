4a9e42b8497271ac3e892cec2ae43499c6369ceb
patrickmontalto <pmontalto@gmail.com>
2015-11-11 21:13:31 -0500

    active record followers and followed

--- active_record/associations_warmup.rb
+++ active_record/associations_warmup.rb
@@ -147,13 +147,19 @@ user table
 id :integer
 username :string
 
-user_followings table
+relationships table
 follower_id :integer
 followed_id :integer
 
 class User < ActiveRecord::Base
-  has_many :followers, :through => :user_followings
-  has_many :followed_users, 
+  has_many :active_relationships, :class_name => "Relationship",
+                                  :foreign_key => "follower_id"
+                                  dependent: :destroy
+  has_many :passive_relationships, :class_name => "Relationship",
+                                   :foreign_key => "followed_id",
+                                   dependent: :destroy
+  has_many :following, :through => :active_relationships, source: :followed
+  has_many :followers, :through => :passive_relationships
 end
 
 class UserFollowings < ActiveRecord::Base
